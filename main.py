33e95dff4e46186d356841517b9ff5da26833bcb
Leo Chen <Leo_Chen_zy@hotmail.cm>
2021-04-26 23:22:25 -0500

    pc test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import urllib.request
 
-response = urllib.request.urlopen('https://www.python.org')
+response = urllib.request.urlopen('https://trends.google.com/trends/explore?q=%2Fm%2F0dl567,%2Fm%2F0261x8t&date=now%207-d&geo=US')
 print(str(response.read(), 'utf-8'))
