3254a77960583e91d9eae48585321f752107d5e0
aaroh <aaroh.gokhale123>
2021-05-01 00:25:55 -0500

    4th Commit

--- Newtonsmethod.py
+++ Newtonsmethod.py
@@ -4,6 +4,9 @@ class NewtonsMethod:
     def __init__(self, coeff_vec):
         self.coeff = coeff_vec
 
+    def set_coeff(self, coeff_vec):
+        self.coeff = coeff_vec
+
     def eval_poly(self, x):
 
         x_ans = 0
@@ -33,8 +36,27 @@ class NewtonsMethod:
         return x_ans
 
     def eval_newton(self, x):
-        return x - (self.eval_poly(x)/self.eval_derivative(x));
+        return x - (self.eval_poly(x)/self.eval_derivative(x))
 
+    def eval_ntimes(self, n, x):
+
+        xnew = x
+
+        for i in range(0,n):
+            xnew = self.eval_newton(xnew)
+
+        return xnew
+
+    def to_string(self):
+
+        poly = ""
+
+        for i in range(0,len(self.coeff)):
+            poly += str(self.coeff[i]) + "x^" + str(len(self.coeff) - 1 - i) + " + "
+
+        poly = poly[:len(poly)-3]
+
+        return "Current polynomial being used: " + poly
 
 
 
