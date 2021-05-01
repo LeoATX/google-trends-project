4affe8e4b11fab5033c7b384186703df9b7110af
Leo Chen <Leo_Chen_zy@hotmail.cm>
2021-05-01 16:46:35 -0500

    Update

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ try:
 except urllib.error.HTTPError:
     print(urllib.error.HTTPError)
 
-print("=========PYTRENDS=========")
 client = pytrends.request.TrendReq(hl='en-US', tz=360)
-client.build_payload(["Hello"])
-print(client.interest_over_time())
+client.build_payload(["Coronavirus"])
+data_frame_object = client.interest_over_time()
+a = data_frame_object.to_csv()
+print(a)
