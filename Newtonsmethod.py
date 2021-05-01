95d492476d48dfde9619eed38cc469498cca9a9e
aaroh <aaroh.gokhale123>
2021-04-30 22:35:02 -0500

    2nd Commit

--- Newtonsmethod.py
+++ Newtonsmethod.py
@@ -9,7 +9,7 @@ class NewtonsMethod:
         return 2*x
 
     def eval_newton(self, x):
-        return x - (self.eval_poly(x)/self.eval_derivative(x))
+        return x - (self.eval_poly(x)/self.eval_derivative(x));
 
 
 
