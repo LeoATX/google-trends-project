55fb345f065d24557018538af826804c3a1679fc
Leo Chen <Leo_Chen_zy@hotmail.cm>
2021-05-01 16:47:16 -0500

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import urllib.error
 import urllib.request
-# import pytrends.request
+import pytrends.request
 
 try:
     response = urllib.request.urlopen('https://trends.google.com/trends/explore?q=%2Fm%2F04n3w2r&geo=US.json')
@@ -11,5 +11,5 @@ except urllib.error.HTTPError:
 client = pytrends.request.TrendReq(hl='en-US', tz=360)
 client.build_payload(["Coronavirus"])
 data_frame_object = client.interest_over_time()
-a = data_frame_object.to_csv()
-print(a)
+csv = data_frame_object.to_csv()
+print(csv)
