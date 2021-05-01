eed4c64da6b11fd7d440be8a9cb657759b15895e
aaroh <aaroh.gokhale123>
2021-05-01 00:05:11 -0500

    3rd Commit

--- Newtonsmethod.py
+++ Newtonsmethod.py
@@ -1,12 +1,36 @@
 
-
 class NewtonsMethod:
 
+    def __init__(self, coeff_vec):
+        self.coeff = coeff_vec
+
     def eval_poly(self, x):
-        return (x*x) - 2
+
+        x_ans = 0
+
+        n = 0
+
+        while n < len(self.coeff):
+
+            x_ans += (self.coeff[n] * (x**(len(self.coeff) - n - 1)))
+
+            n += 1
+
+        return x_ans
 
     def eval_derivative(self, x):
-        return 2*x
+
+        x_ans = 0
+
+        n = 0
+
+        while n < len(self.coeff)-1:
+
+            x_ans += x_ans + (self.coeff[n] * (len(self.coeff) - n - 1) * (x**(len(self.coeff) - n - 2)))
+
+            n += 1
+
+        return x_ans
 
     def eval_newton(self, x):
         return x - (self.eval_poly(x)/self.eval_derivative(x));
