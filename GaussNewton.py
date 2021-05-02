8b0bb1d19081629e47d3a4f4099731cd3e3e6051
aaroh <aaroh.gokhale123>
2021-05-01 21:24:07 -0500

    6th commit

--- GaussNewton.py
+++ GaussNewton.py
@@ -1,2 +1,17 @@
-print("I just learned the Gauss Newton algorithm, an algorithm that uses partial derivatives in order to approximate"
-      "a given set of data ")
+class GaussNewton:
+
+    def __init__(self, table):
+        self.dataset = table
+
+    def jacobian(self, n):
+        j = []
+
+        for i in range(0, len(self.dataset)):
+            new_vec = []
+            for j in range(0, n + 1):
+                new_vec.append(self.dataset[i][0] ** j)
+
+            j.append(new_vec)
+
+        return j
+
