3724d3bd300062052b2471385c7ee883045b8b13
Leo Chen <Leo_Chen_zy@hotmail.cm>
2021-05-02 11:14:53 -0500

    Name change for gauss newton

--- newtonsmethod.py
+++ newtonsmethod.py
@@ -1,4 +1,3 @@
-
 class NewtonsMethod:
 
     def __init__(self, coeff_vec):
@@ -14,8 +13,7 @@ class NewtonsMethod:
         n = 0
 
         while n < len(self.coeff):
-
-            x_ans += (self.coeff[n] * (x**(len(self.coeff) - n - 1)))
+            x_ans += (self.coeff[n] * (x ** (len(self.coeff) - n - 1)))
 
             n += 1
 
@@ -27,22 +25,21 @@ class NewtonsMethod:
 
         n = 0
 
-        while n < len(self.coeff)-1:
-
-            x_ans += x_ans + (self.coeff[n] * (len(self.coeff) - n - 1) * (x**(len(self.coeff) - n - 2)))
+        while n < len(self.coeff) - 1:
+            x_ans += x_ans + (self.coeff[n] * (len(self.coeff) - n - 1) * (x ** (len(self.coeff) - n - 2)))
 
             n += 1
 
         return x_ans
 
     def eval_newton(self, x):
-        return x - (self.eval_poly(x)/self.eval_derivative(x))
+        return x - (self.eval_poly(x) / self.eval_derivative(x))
 
     def eval_ntimes(self, n, x):
 
         xnew = x
 
-        for i in range(0,n):
+        for i in range(0, n):
             xnew = self.eval_newton(xnew)
 
         return xnew
@@ -51,23 +48,9 @@ class NewtonsMethod:
 
         poly = ""
 
-        for i in range(0,len(self.coeff)):
+        for i in range(0, len(self.coeff)):
             poly += str(self.coeff[i]) + "x^" + str(len(self.coeff) - 1 - i) + " + "
 
-        poly = poly[:len(poly)-3]
+        poly = poly[:len(poly) - 3]
 
         return "Current polynomial being used: " + poly
-
-
-
-
-
-
-
-
-
-
-
-
-
-
