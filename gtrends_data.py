d4039a3a332661c8a6ce353ffca8ee5906258bdb
Leo Chen <Leo_Chen_zy@hotmail.cm>
2021-05-09 18:27:42 -0500

    Update by Leo

---
+++ gtrends_data.py
@@ -0,0 +1,49 @@
+import pytrends.request
+
+
+def get(keyword: str):
+    client = pytrends.request.TrendReq(hl='en-US', tz=360)
+    client.build_payload([keyword])
+    data_frame_object = client.interest_over_time()
+    fetched_csv = data_frame_object.to_csv()
+
+    fetched_csv = str(fetched_csv)
+
+    # Create a list to iterate the csv
+    csv_iterable = list()
+    csv_iterable.append('')
+
+    # Loop through the csv file and add every row into csv_iterable
+    row_count = 0
+    for character in fetched_csv:
+        if character != '\n':
+            csv_iterable[row_count] += character
+        if character == '\n':
+            row_count += 1
+            csv_iterable.append('')
+
+    # Delete the last element
+    del csv_iterable[len(csv_iterable) - 1]
+
+    # Loop through each row and grab the value from the middle
+    list_values = list()
+    row_count = 0
+    for row in csv_iterable:
+        # Get rid of the first row
+        if row_count == 0:
+            row_count += 1
+            continue
+
+        row_list = list()
+        row_list.append('')
+        mini_list_count = 0
+        for character in row:
+            if character != ',':
+                row_list[mini_list_count] += character
+            if character == ',':
+                row_list.append('')
+                mini_list_count += 1
+
+        list_values.append(row_list[1])
+
+    return list_values
--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-import urllib.error
-import urllib.request
-import pytrends.request
-import csv
-
-try:
-    response = urllib.request.urlopen('https://trends.google.com/trends/explore?q=%2Fm%2F04n3w2r&geo=US.json')
-    print(response)
-except urllib.error.HTTPError:
-    print(urllib.error.HTTPError)
-
-client = pytrends.request.TrendReq(hl='en-US', tz=360)
-client.build_payload(["Coronavirus"])
-data_frame_object = client.interest_over_time()
-fetched_csv = data_frame_object.to_csv()
-
-spamreader = csv.reader(fetched_csv)
-for row in spamreader:
-    print(', '.join(row))
-
-print(spamreader)
+import gtrends_data
+# This will return a list
+print(gtrends_data.get('Coronavirus'))
