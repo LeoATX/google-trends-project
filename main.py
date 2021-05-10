ff3387d7b5fcd6be699f3ec2da073d5322ff9f14
aaroh <aaroh.gokhale123>
2021-05-09 20:39:59 -0500

    Main edit

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import gtrends_data
+from gauss_newton import GaussNewton
 # This will return a list
-print(gtrends_data.get('Coronavirus'))
+covidlist = gtrends_data.get("Coronavirus")
+print(len(covidlist))
+print(covidlist)
+
+data = []
+w = 0
+for i in covidlist:
+    data.append([w, int(i)])
+    w+=1
+
+print(data)
+
+gn = GaussNewton(data)
+
+n = 10
+beta = [1, 1, 1, 1, 1, 1, 1]
+for i in range(0,n):
+    print(gn.gauss_newton(beta))
+    beta = gn.gauss_newton(beta)
